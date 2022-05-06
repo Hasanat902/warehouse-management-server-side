fa75f6a2a041f45b1dd088151e7a4d311595522a
Hasanat902 <abulhasanat902@gmail.com>
2022-05-06 23:53:12 +0600

    created get api and load products

--- index.js
+++ index.js
@@ -12,14 +12,27 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://{process.env.DB_USER}:{process.env.DB_PASS}@cluster0.3ab0f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3ab0f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("db connected");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("wareHouse").collection("products");
+
+        app.get('/product', async(req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 
 
